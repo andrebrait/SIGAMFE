3790a50d8ebc4c6ade6658b3d9284e3713650d88
Andre <Andre@SVBRAIT>
2015-04-27 23:32:54 -0300

    Rename

--- sigamf/src/main/java/com/sigamfe/App.java
+++ sigamf/src/main/java/com/sigamfe/App.java
@@ -1,4 +1,4 @@
-package com.sigamf;
+package com.sigamfe;
 
 /**
  * Hello world!
--- sigamf/src/test/java/com/sigamfe/AppTest.java
+++ sigamf/src/test/java/com/sigamfe/AppTest.java
@@ -1,4 +1,4 @@
-package com.sigamf;
+package com.sigamfe;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
