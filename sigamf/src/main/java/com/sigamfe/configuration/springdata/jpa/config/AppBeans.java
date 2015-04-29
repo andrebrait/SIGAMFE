5d28bd93b0956cff98d950d4f418c72253c7124f
Andre Brait <andrebrait@gmail.com>
2015-04-29 18:38:39 -0300

    Beans

--- sigamf/src/main/java/com/sigamfe/configuration/springdata/jpa/config/AppBeans.java
+++ sigamf/src/main/java/com/sigamfe/configuration/springdata/jpa/config/AppBeans.java
@@ -1,5 +1,29 @@
 package com.sigamfe.configuration.springdata.jpa.config;
 
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.hibernate4.encryptor.HibernatePBEStringEncryptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppBeans {
+
+	@Bean
+	private PooledPBEStringEncryptor strongEncryptor() {
+		final PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+		encryptor.setAlgorithm("PBEWithMD5AndTripleDES");
+		encryptor.setPassword("SIGAMFE_Pass_###71662####$%$%");
+		encryptor.setPoolSize(4);
+		return new PooledPBEStringEncryptor();
+	}
+
+	@Bean
+	public HibernatePBEStringEncryptor hibernateStringEncryptor() {
+		final HibernatePBEStringEncryptor hibEncryptor = new HibernatePBEStringEncryptor();
+		hibEncryptor.setRegisteredName("strongHibernateStringEncryptor");
+		hibEncryptor.setEncryptor(strongEncryptor());
+		return hibEncryptor;
+
+	}
 
 }
