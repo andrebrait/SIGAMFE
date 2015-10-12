02d60713698aa854c0e37ffe98f687f75263a8cb
andre <andre@Andre-Vostro.lan>
2015-10-11 22:06:53 -0300

    Geração automática de Metamodels consertada

--- sigamf/src/main/java/com/sigamfe/configuration/PersistenceConfiguration.java
+++ sigamf/src/main/java/com/sigamfe/configuration/PersistenceConfiguration.java
@@ -1,12 +1,10 @@
 package com.sigamfe.configuration;
 
+import java.sql.SQLException;
 import java.util.Properties;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
-
-import lombok.Getter;
-import lombok.Setter;
 
 import org.mariadb.jdbc.MySQLDataSource;
 import org.springframework.context.annotation.Bean;
@@ -19,8 +17,12 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import com.sigamfe.exception.GenericException;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * The Class PersistenceContext.
@@ -34,12 +36,18 @@ public class PersistenceConfiguration {
 	@Setter
 	private static String DB_HOSTNAME, DB_PORT;
 
-	private static String DB_USERNAME = "sigamfe", DB_PASSWORD = "TEHASi", AUTOCONNECTION_TEST_QUERY = "SELECT 1", DB_DATABASE = "sigamfe";
+	private static String DB_USERNAME = "sigamfeuser", DB_PASSWORD = "TEHASi", AUTOCONNECTION_TEST_QUERY = "SELECT 1",
+			DB_DATABASE = "sigamfe";
 	private static boolean AUTO_COMMIT = true;
 
 	@Bean(destroyMethod = "close")
 	public DataSource dataSource() {
-		DataSource datasource = new MySQLDataSource(DB_HOSTNAME, Integer.parseInt(DB_PORT), DB_DATABASE);
+		DataSource datasource;
+		try {
+			datasource = new MySQLDataSource(DB_HOSTNAME, Integer.parseInt(DB_PORT), DB_DATABASE);
+		} catch (NumberFormatException | SQLException e) {
+			throw new GenericException("Erro de conexão ao banco de dados. Motivo: " + e.getMessage());
+		}
 		HikariConfig hikariConfig = new HikariConfig();
 		hikariConfig.setDataSource(datasource);
 		hikariConfig.setUsername(DB_USERNAME);
@@ -50,9 +58,10 @@ public class PersistenceConfiguration {
 		return new HikariDataSource(hikariConfig);
 	}
 
-	private static String PACKAGES_TO_SCAN = "com.sigamfe.model", HIBERNATE_DIALECT = "org.hibernate.dialect.MySQL5InnoDBDialect",
-			HIBERNATE_EJB_NAMING_STRATEGY = "org.hibernate.cfg.ImprovedNamingStrategy", HIBERNATE_SHOW_SQL = "false", HIBERNATE_FORMAT_SQL = "true",
-			HIBERNATE_HBM2DDL_AUTO = "validate";
+	private static String PACKAGES_TO_SCAN = "com.sigamfe.model",
+			HIBERNATE_DIALECT = "org.hibernate.dialect.MySQL5InnoDBDialect",
+			HIBERNATE_EJB_NAMING_STRATEGY = "org.hibernate.cfg.ImprovedNamingStrategy", HIBERNATE_SHOW_SQL = "false",
+			HIBERNATE_FORMAT_SQL = "true", HIBERNATE_HBM2DDL_AUTO = "validate";
 
 	@Bean
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
